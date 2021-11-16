250d5d6d7abe67568d3322dd8d16c9c7bb09caa1
CaminoR <75613356+CaminoRuano@users.noreply.github.com>
2021-11-16 13:56:40 +0100

    Update ploting_GA_Cellcomp.R

--- ploting_GA_Cellcomp.R
+++ ploting_GA_Cellcomp.R
@@ -2,14 +2,21 @@ library(ggplot2)
 library(ggpubr)
 library(ggarrange)
 
+setwd("")
+
+## set parameters
 my_comparisons = list(c("Control","Diseased"))
+my_color = c("#f47b8e", "#c578eb", "#eddf8e","#5ebde5", "#85f37b", '#bcbcbc', '#5b5b5b')
+my_breaks = c("CTRL", "chorioamnionitis", "IUGR", "PE", "PE_IUGR", "Control", "Diseased")
+
+# create plots 
 
 p <- ggplot(cell, aes(x = pat, y = STB)) + ggtitle("Syncytiotrophoblast") +
   ylab("Percentage Syncytiotrophoblast")+
   theme(plot.title =element_text(hjust = 0.5, size=25)) + 
   geom_boxplot(outlier.shape = NA, aes(fill=pat))+
-  scale_fill_manual(breaks = c("CTRL", "chorioamnionitis", "IUGR", "PE", "PE_IUGR", "Control", "Diseased"),
-                    values=c("#f47b8e", "#c578eb", "#eddf8e","#5ebde5", "#85f37b", '#bcbcbc', '#5b5b5b'))+
+  scale_fill_manual(breaks = my_breaks,
+                    values=my_color)+
   ylim(0, 0.8)+
   stat_compare_means(pat~STB, data=cell, method="t.test", paired=FALSE, comparisons=my_comparisons, size=8,label="p.signif")+
   geom_jitter(size=2,
@@ -25,8 +32,8 @@ pt <- ggplot(cell, aes(x = pat, y = TB)) + ggtitle("Trophoblast") +
   ylab("Percentage Trophoblast")+
   theme(plot.title =element_text(hjust = 0.5, size=25)) + 
   geom_boxplot(outlier.shape = NA, aes(fill=pat))+
-  scale_fill_manual(breaks = c("CTRL", "chorioamnionitis", "IUGR", "PE", "PE_IUGR", "Control", "Diseased"),
-                    values=c("#f47b8e", "#c578eb", "#eddf8e","#5ebde5", "#85f37b", '#bcbcbc', '#5b5b5b'))+
+  scale_fill_manual(breaks = my_breaks,
+                    values=my_color)+
   ylim(0, 0.45)+
   stat_compare_means(pat~TB, data=cell, method="t.test", paired=FALSE, comparisons=my_comparisons, size=8,label="p.signif")+
   geom_jitter(size=2,
@@ -93,8 +100,8 @@ ps<- ggplot(cell, aes(x = pat, y = Stromal)) + ggtitle("Stromal") +
   ylab("Percentage Stromal")+
   theme(plot.title =element_text(hjust = 0.5, size=25)) + 
   geom_boxplot(outlier.shape = NA, aes(fill=pat))+
-  scale_fill_manual(breaks = c("CTRL", "chorioamnionitis", "IUGR", "PE", "PE_IUGR", "Control", "Diseased"),
-                    values=c("#f47b8e", "#c578eb", "#eddf8e","#5ebde5", "#85f37b", '#bcbcbc', '#5b5b5b'))+
+  scale_fill_manual(breaks = my_breaks,
+                    values=cmy_color)+
   ylim(0, 0.25)+
   stat_compare_means(pat~Stromal, data=cell, method="t.test", paired=FALSE, comparisons=my_comparisons, size=8,label="p.signif")+
   geom_jitter(size=2,
@@ -105,7 +112,7 @@ ps<- ggplot(cell, aes(x = pat, y = Stromal)) + ggtitle("Stromal") +
         axis.line = element_line(colour = "black"), panel.background = element_rect(fill = "white"))
 
 
-
+## merge plots in a single figure
 
 plot =ggarrange(p, pe, pm, pr, ps, pt, ncol=2, nrow=3,common.legend = TRUE, legend="left")
 
